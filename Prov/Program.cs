31ccb34a01ef2a4a196067234ddc14024d242759
Leo Eriksson <leo.eriksson5@learnet.se>
2020-10-05 09:46:09 +0200

    Kommentar commit

--- Prov/Program.cs
+++ Prov/Program.cs
@@ -8,13 +8,13 @@ namespace Prov
         {
             bool spel = true;
             Random generator = new Random();
-            int rattSvar = generator.Next(1, 11);
-            int forsok = 0;
-            string svar;
-            int nummerSvar = 0;
+            int rattSvar = generator.Next(1, 11); //Slumpmässigt nummer
+            int forsok = 0;  //försök int
+            string svar;  //svar string
+            int nummerSvar = 0; // svar i nummer form
             Console.WriteLine("Gissa på en kordinat mellan 1 och 10");
 
-            while (spel == true)
+            while (spel == true) //kör medans svaret inte är rätt
             {
                 svar = Console.ReadLine();
 
@@ -29,7 +29,7 @@ namespace Prov
 
                 forsok++;
 
-                if (nummerSvar <= 0 || nummerSvar >= 11)
+                if (nummerSvar <= 0 || nummerSvar >= 11)// om svaret är mindre än 1 eller större än 10 måste man gissa igen
                 {
                     Console.WriteLine("Jag sa mellan 1 och 10!");
                 }
@@ -40,12 +40,13 @@ namespace Prov
                         Console.WriteLine("Hit!");
                         Console.WriteLine("Det tog dig " + forsok + " försök!");
                     }
-                    Console.WriteLine("Vill du spela igen? \"JA\" eller \"NEJ\"");
+                    Console.WriteLine("Vill du spela igen? \"JA\" eller \"NEJ\""); // kollar om spelaren vill spela igen
                     string startaOm = Console.ReadLine();
                     if (startaOm == "JA")
                     {
                         rattSvar = generator.Next(1, 11);
                         Console.WriteLine("Gissa på en kordinat mellan 1 och 10");
+                        forsok = 0;
                     }
                     if (startaOm == "NEJ")
                     {
